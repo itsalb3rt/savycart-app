0852a08b23be194034c0c487c1dd837623f78dc3
Albert E. Hidalgo Taveras <itsalb3rt@gmail.com>
2020-03-13 20:38:24 -0400

    added properties and methods for user authentication

--- src/store/Modules/Auth/Auth.js
+++ src/store/Modules/Auth/Auth.js
@@ -2,6 +2,9 @@ import httpClient from '@/api/HttpClient'
 
 export default {
     namespaced: true,
+    state:{
+        isLogged: false,
+    },
     actions: {
         login({
             commit
@@ -12,6 +15,16 @@ export default {
             commit
         }, payload) {
             return httpClient.post(`/auth/register`, payload)
+        }
+    },
+    getters:{
+        getIsLogged(state){
+            return state.isLogged;
+        }
+    },
+    mutations:{
+        SET_LOGGED(state,value){
+            state.isLogged = value;
         }
     }
 }
