4b64a940d9dbd67bb02f75cdc7756f62fccf721a
Albert E. Hidalgo Taveras <itsalb3rt@gmail.com>
2020-03-19 18:40:19 -0400

    refactor

--- src/views/auth/register.vue
+++ src/views/auth/register.vue
@@ -133,79 +133,51 @@ export default {
   computed: {
     ...mapState([ "user"]),
     isUserNameValid(){
-      return /^[a-z0-9_-]{3,15}$/.test(this.userName);
+      return /^[a-z0-9_-]{3,15}$/.test(this.newUser.user_name);
     }
   },
   methods: {
     validatedUserName() {
-      let route = `${this.apiDomain}/auth/userAlreadyExists/${this.userName}`;
-      this.axios({
-        method: "get",
-        url: route
-      })
+      this.$store.dispatch('auth/checkIsUserExists',{userName:this.newUser.user_name})
         .then(response => {
-          if (response.data.status == "true") {
-            this.userExitst = true;
-          } else {
+          this.userExitst = true;
+        })
+        .catch( error => {
+          if(error.response.status === 404){
             this.userExitst = false;
           }
-        })
-        .catch(function(error) {
-          console.log(error);
         });
     },
     validatedEmail() {
-      let route = `${this.apiDomain}/auth/emailAlreadyExists`;
-      let data = new FormData();
-      data.append("email", this.email);
-      this.axios({
-        method: "POST",
-        url: route,
-        data: data
-      })
+      this.$store.dispatch('auth/checkIsEmailExists',{email:this.newUser.email})
         .then(response => {
-          if (response.data.status == "true") {
-            this.emailExits = true;
-          } else {
+          this.emailExits = true;
+        })
+        .catch( error => {
+          if(error.response.status === 404){
             this.emailExits = false;
           }
-        })
-        .catch(function(error) {
-          console.log(error);
         });
     },
     registerUser() {
       this.disabledSubmitButton = true;
 
-      data.append("first_name", this.firstName);
-      data.append("last_name", this.lastName);
-      data.append("user_name", this.userName);
-      data.append("email", this.email);
-      data.append("password", this.password);
-      data.append("password2", this.password2);
-
-      this.$store.dispatch('auth/register')
+      this.$store.dispatch('auth/register', this.newUser)
         .then(response => {
-          if (response.data.status == "register") {
             this.$router.push({
               path: "login",
               query: { create_user: "created" }
             });
-          } else if (response.data.status == "user_exists") {
-            this.disabledSubmitButton = false;
-            this.userExitst = true;
-          } else if (response.data.status == "email_exists") {
-            this.disabledSubmitButton = false;
-            this.emailExits = true;
-          }
         })
         .catch(function(error) {
-          this.disabledSubmitButton = false;
+          
           console.log(error);
-        });
+        }).finally(()=>{
+          this.disabledSubmitButton = false;
+        })
     },
     purifyUserName(){
-      this.userName = this.userName.toLowerCase();
+      this.newUser.user_name = this.newUser.user_name.toLowerCase();
     }
   }
 };
