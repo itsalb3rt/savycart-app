1abe0c039cd5dc83c34af1827a05baf7c5de81cd
Albert E. Hidalgo Taveras <itsalb3rt@gmail.com>
2020-03-21 17:20:24 -0400

    vuetify refactor

--- src/views/products/list.vue
+++ src/views/products/list.vue
@@ -1,96 +1,75 @@
 <template>
 	<div>
-		<v-layout row wrap>
-			<v-flex xs12>
-				<v-card flat>
-					<v-card-text>
-						<v-flex xs12 >
-							<v-text-field
-								prepend-inner-icon="fa-search"
-								v-model="searchProductName"
-								:label=" $t('products.search') + '...' "
-								clearable
-							></v-text-field>
-							<v-tabs fixed-tabs>
-								<v-tab @click="showFavorites = false">
-									<v-icon class="mr-2">fa-list</v-icon>
-									{{ $t('products.all') }}
-								</v-tab>
-								<v-tab @click="showFavorites = true">
-									<v-icon class="mr-2">fa-star</v-icon>
-									{{ $t('products.favorites') }}
-								</v-tab>
-							</v-tabs>
-						</v-flex>
-						<v-flex xs12 class="mt-3" v-if="filterProducts.length > 0">
-							<v-layout class="mt-2 mb-2" row wrap v-for="(product,index) in filterProducts" :key="index">
-								<v-flex xs12>
-									<v-card>
-										<v-card-text>
-											<v-flex xs12>
-												<div>
-													<span
-														@click="$router.push({ name: 'view_product', params: { id: product.id_product } })"
-														flat
-														small
-														class="ma-0 pa-0 primary--text font-weight-bold subheading"
-													>{{product.name}}</span>
-													<span v-if="product.favorite == '1' ">
-														<v-icon small color="warning" class="ml-2">fa-star</v-icon>
-													</span>
-												</div>
-												<div class="grey--text">{{getMeasurementName(product.id_unit_measurement)}}</div>
-												<div class="font-weight-bold subheading">{{currency.symbol}} {{product.price}}</div>
-											</v-flex>
-										</v-card-text>
-										<v-card-actions>
-											<v-btn
-												color="success"
-												small
-												depressed
-												outlined
-												@click="$router.push({name:'edit product', params:{id: product.id_product} })"
-											>
-												<v-icon small class="mr-1">fa-edit</v-icon>
-												{{ $t('call_action_buttons.edit') }}
-											</v-btn>
-											<v-spacer></v-spacer>
-											<v-btn
-												color="error"
-												small
-												text
-												class="ma-0 pa-0 right"
-												@click="showDialogToDeleteProduct(product.id_product)"
-											>
-												<v-icon small class="mr-1">fa-trash</v-icon>
-												{{ $t('call_action_buttons.delete') }}
-											</v-btn>
-										</v-card-actions>
-									</v-card>
-								</v-flex>
-							</v-layout>
-						</v-flex>
-						<v-flex v-if="filterProducts.length == 0" xs12>
-							<p class="headline mt-5 grey--text font-weight-bold">{{ $t('products.empty_list') }}</p>
-						</v-flex>
-						<v-btn
-							color="primary"
-							dark
-							fixed
-							right
-							bottom
-							fab
-							@click="$router.push({path:'/product/add'})"
-						>
-							<v-icon>fa-plus</v-icon>
-						</v-btn>
-					</v-card-text>
-				</v-card>
-			</v-flex>
-		</v-layout>
-		<v-layout row justify-center>
-			<delete-dialog :show="dialog" @cancel="dialog = false" @confirm="deleteProduct()" />
-		</v-layout>
+		<v-row>
+			<v-col cols="12">
+				<v-text-field
+					prepend-inner-icon="fa-search"
+					v-model="searchProductName"
+					:label=" $t('products.search') + '...' "
+					clearable
+				></v-text-field>
+				<v-tabs fixed-tabs>
+					<v-tab @click="showFavorites = false">
+						<v-icon class="mr-2">fa-list</v-icon>
+						{{ $t('products.all') }}
+					</v-tab>
+					<v-tab @click="showFavorites = true">
+						<v-icon class="mr-2">fa-star</v-icon>
+						{{ $t('products.favorites') }}
+					</v-tab>
+				</v-tabs>
+			</v-col>
+			<v-col cols="12" v-if="filterProducts.length > 0">
+				<v-row v-for="(product,index) in filterProducts" :key="index">
+					<v-col cols="12">
+						<v-card>
+							<v-card-text>
+								<div>
+									<span
+										@click="$router.push({ name: 'view_product', params: { id: product.id_product } })"
+										text
+										class="ma-0 pa-0 primary--text font-weight-bold subheading"
+									>{{product.name}}</span>
+									<span v-if="product.favorite == '1' ">
+										<v-icon color="warning" class="ml-2">fa-star</v-icon>
+									</span>
+								</div>
+								<div class="grey--text">{{getMeasurementName(product.id_unit_measurement)}}</div>
+								<div class="font-weight-bold subheading">{{currency.symbol}} {{product.price}}</div>
+							</v-card-text>
+							<v-card-actions>
+								<v-btn
+									color="success"
+									depressed
+									outlined
+									@click="$router.push({name:'edit product', params:{id: product.id_product} })"
+								>
+									<v-icon class="mr-1">fa-edit</v-icon>
+									{{ $t('call_action_buttons.edit') }}
+								</v-btn>
+								<v-spacer></v-spacer>
+								<v-btn
+									color="error"
+									text
+									class="ma-0 pa-0 right"
+									@click="showDialogToDeleteProduct(product.id_product)"
+								>
+									<v-icon class="mr-1">fa-trash</v-icon>
+									{{ $t('call_action_buttons.delete') }}
+								</v-btn>
+							</v-card-actions>
+						</v-card>
+					</v-col>
+				</v-row>
+			</v-col>
+			<v-col v-if="filterProducts.length == 0" cols="12">
+				<p class="headline mt-5 grey--text font-weight-bold">{{ $t('products.empty_list') }}</p>
+			</v-col>
+		</v-row>
+		<v-btn color="primary" dark fixed right bottom fab @click="$router.push({path:'/product/add'})">
+			<v-icon>fa-plus</v-icon>
+		</v-btn>
+		<delete-dialog :show="dialog" @cancel="dialog = false" @confirm="deleteProduct()" />
 		<v-snackbar v-model="successDeleteProduct" color="success">
 			{{ $t('products.product_removed') }}
 			<v-btn dark flat @click="successDeleteProduct = false">{{ $t('call_action_buttons.close') }}</v-btn>
@@ -111,7 +90,6 @@ export default {
 		deleteDialog
 	},
 	async mounted() {
-		
 		this.products = this.$store.getters['products/getProducts'];
 
 		if (this.online) {
@@ -143,9 +121,7 @@ export default {
 				this.searchProductName = '';
 			}
 			let filteredProducts = this.products.filter(item =>
-				item.name
-					.toUpperCase()
-					.includes(this.searchProductName.toUpperCase())
+				item.name.toUpperCase().includes(this.searchProductName.toUpperCase())
 			);
 
 			if (this.showFavorites) {
@@ -182,9 +158,7 @@ export default {
 					this.dialog = false;
 					const productIndexInStore = this.$store.getters[
 						'products/getProducts'
-					].findIndex(
-						product => product.id_product === this.deleteProductId
-					);
+					].findIndex(product => product.id_product === this.deleteProductId);
 					const productIndexInView = this.products.findIndex(
 						product => product.id_product === this.deleteProductId
 					);
@@ -217,10 +191,7 @@ export default {
 			this.$store
 				.dispatch('measurementUnits/getAll')
 				.then(response => {
-					this.$store.commit(
-						'measurementUnits/SET',
-						response.data.data
-					);
+					this.$store.commit('measurementUnits/SET', response.data.data);
 				})
 				.catch(error => {
 					console.log(error);
