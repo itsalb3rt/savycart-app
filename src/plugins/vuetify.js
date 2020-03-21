999113f6e75e806612e2e917ee26ae89ce37a8db
Albert E. Hidalgo Taveras <itsalb3rt@gmail.com>
2020-03-21 16:11:34 -0400

    vuetify refactor

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -8,14 +8,16 @@ export default new Vuetify({
   icons: {
     iconfont: 'fa',
   },
-  theme: {
-    primary: '#03A9F4',
-    secondary: '#607D8B',
-    accent: '#82B1FF',
-    error: '#d32f2f',
-    info: '#17a2b8',
-    success: '#4CAF50',
-    warning: '#FFB300'
+  themes: {
+    light:{
+      primary: '#03A9F4',
+      secondary: '#607D8B',
+      accent: '#82B1FF',
+      error: '#d32f2f',
+      info: '#17a2b8',
+      success: '#4CAF50',
+      warning: '#FFB300'
+    }
   },
   options: {
     customProperties: true
