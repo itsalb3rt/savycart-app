d5f5a318c8496bcffb29129c4d55dd43befeae8b
Albert E. Hidalgo Taveras <itsalb3rt@gmail.com>
2020-04-09 23:56:38 -0400

    fix bug on uppercase brand

--- src/views/products/add.vue
+++ src/views/products/add.vue
@@ -138,7 +138,7 @@ export default {
 				include_tax: this.tax,
 				description: this.description,
 				favorite: this.favorite,
-				brand: this.brand.toUpperCase()
+				brand: (this.brand)? this.brand.toUpperCase() : this.brand
 			};
 
 			if (this.online) {
