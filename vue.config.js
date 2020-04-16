86e315976a0c6fc0a2d82d6c5ca0a96849f94b30
Albert E. Hidalgo Taveras <itsalb3rt@gmail.com>
2020-04-16 17:27:10 -0400

    modification for build

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  publicPath: (process.env.VUE_APP_ON_ROOT_DIR) ? '/' : '/sheiley_shop/',
+  publicPath: (process.env.VUE_APP_ON_ROOT_DIR === true) ? '/' : '/sheiley_shop/',
 
   pluginOptions: {
     i18n: {
