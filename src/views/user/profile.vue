025a98c750d90888f2ff6baaa432659104788795
Albert Hidalgo <alhidalgodev@gmail.com>
2022-07-02 13:06:47 -0400

    refactor
    * Removed loading overlay style

--- src/views/user/profile.vue
+++ src/views/user/profile.vue
@@ -59,7 +59,6 @@
 <script>
 import { mapMutations, mapState } from 'vuex';
 import Loading from 'vue-loading-overlay';
-import 'vue-loading-overlay/dist/vue-loading.css';
 import OfflineInfomation from '@/components/Offline/OfflineInformation';
 
 export default {
