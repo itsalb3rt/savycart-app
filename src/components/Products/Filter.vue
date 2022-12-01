dbf5482cc2821e374c1e876fbb0fb1d5ccaf1d29
Albert Hidalgo <alhidalgodev@gmail.com>
2022-11-30 20:13:09 -0400

    refactor
    * Changed buttons for radio buttons

--- src/components/Products/Filter.vue
+++ src/components/Products/Filter.vue
@@ -1,23 +1,21 @@
 <template>
   <v-bottom-sheet v-model="localShow" inset>
-    <v-sheet style="border-radius: 8px" height="200px">
-      <v-card outlined>
-        <v-card-text> Sort by </v-card-text>
-        <v-card-text>
-          <v-btn-toggle v-model="sortBy" color="primary" mandatory>
-            <v-btn value="nameAZ">Name AZ</v-btn>
-            <v-btn value="nameZA">Name ZA</v-btn>
-            <v-btn value="dateNew">Date New</v-btn>
-            <v-btn value="dateOld">Date Old</v-btn>
-          </v-btn-toggle>
-        </v-card-text>
-        <v-card-actions>
-          <v-btn @click="handleSort" tyle="margin:auto" color="success">
-            <v-icon class="mr-1">fa-check</v-icon>
-            {{ $t('call_action_buttons.done') }}
-          </v-btn>
-        </v-card-actions>
-      </v-card>
+    <v-sheet style="border-radius: 8px" height="280px">
+      <v-card-text class="pb-0 font-weight-bold"> Sort by </v-card-text>
+      <v-card-text class="pt-0">
+        <v-radio-group v-model="sortBy">
+          <v-radio :label="$t('products.nameAsc')" value="nameAZ"> </v-radio>
+          <v-radio :label="$t('products.nameDesc')" value="nameZA"> </v-radio>
+          <v-radio :label="$t('products.oldToNew')" value="dateNew"> </v-radio>
+          <v-radio :label="$t('products.newToOld')" value="dateOld"> </v-radio>
+        </v-radio-group>
+      </v-card-text>
+      <div class="text-center">
+        <v-btn @click="handleSort" tyle="margin:auto" color="success">
+          <v-icon class="mr-1">fa-check</v-icon>
+          {{ $t('call_action_buttons.done') }}
+        </v-btn>
+      </div>
     </v-sheet>
   </v-bottom-sheet>
 </template>
@@ -33,7 +31,7 @@ export default {
   data() {
     return {
       localShow: this.show,
-      sortBy: 'name',
+      sortBy: 'nameAZ',
     };
   },
   methods: {
