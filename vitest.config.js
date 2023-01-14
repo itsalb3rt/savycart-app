99ea821828f662cab060f871619cd97e20477f6a
Albert Hidalgo <alhidalgodev@gmail.com>
2023-01-13 22:04:47 -0400

    feat
    * Added alias

--- vitest.config.js
+++ vitest.config.js
@@ -11,6 +11,7 @@ export default defineConfig({
       'components': path.resolve(__dirname, 'src/components'),
       'assets': path.resolve(__dirname, './src/assets'),
       'tests': path.resolve(__dirname, './tests'),
+      '@': path.resolve(__dirname, './src'),
     },
   },
   test: {
