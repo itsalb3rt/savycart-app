e92337091c7425014c31d13847d523a23366a240
Albert Hidalgo <itsalb3rt@gmail.com>
2023-08-05 15:22:34 -0400

    feat
    * Prepare the env for ios build

---
+++ mobileBuild.js
@@ -0,0 +1,12 @@
+// This file only remove the url from the server object into the capacitor config file
+const fs = require('fs');
+const fileName = './capacitor.config.json';
+const file = require(fileName);
+
+var ip = require("ip");
+
+delete file.server.url
+
+fs.writeFile(fileName, JSON.stringify(file), function writeJSON(err) {
+  if (err) return console.log(err);
+});
